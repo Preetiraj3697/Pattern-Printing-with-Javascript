15ee33ca8a0d52ddf524f2652c5aa98129ba3fa1
Preeti Raj <107460745+Preetiraj3697@users.noreply.github.com>
2023-01-05 11:45:50 +0530

    add

---
+++ ABCDESquare.js
@@ -0,0 +1,17 @@
+// A B C D E
+// A B C D E
+// A B C D E
+// A B C D E
+// A B C D E
+//First Approach
+let output = '';
+for (let i = 0; i < 5; i++) {
+  output += 'A B C D E\n';
+}
+console.log(output);
+//second Approach
+// for (let i = 0; i < 5; i++) {
+//     console.log('A B C D E');
+//   }
+//Third Approach
+// console.log('A B C D E\nA B C D E\nA B C D E\nA B C D E\nA B C D E');
